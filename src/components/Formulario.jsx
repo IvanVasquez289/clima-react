d7b74afb4c9083cdabea30be688fc11d22f1fd2e
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-18 22:59:08 -0600

    Creando el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className='contenedor'>
+        <form>
+            <div className="campo">
+                <label htmlFor="ciudad">Ciudad</label>
+                <input 
+                    type="text" 
+                    name="ciudad" 
+                    id="ciudad" 
+                    placeholder='Escribe tu ciudad'
+                />
+            </div>
+            <div className="campo">
+                <label htmlFor="pais">Pais</label>
+                <select name="ciudad" id="ciudad">
+                    <option value="">Seleccione un pais</option>
+                    <option value="MX">Mexico</option>
+                    <option value="EU">Estados Unidos</option>
+                    <option value="CO">Colombia</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                  
+                </select>
+            </div>
+
+            <input type="submit" value="Consultar Clima" />
+        </form>
+    </div>
   )
 }
 
